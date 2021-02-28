e9bf33b37349309b7aa4aebceeb75f2720a37b9a
Imran Sajeb <43618577+imransajeb@users.noreply.github.com>
2021-03-01 00:21:59 +0600

    JavaScript Fundamental part - 1

---
+++ 01/script.js
@@ -0,0 +1,91 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('imran');
+console.log(23);
+
+let firstName = 'Metelda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas"
+let PI = 3.1415;
+
+
+
+
+let javascriptIsfun = true;
+console.log(true);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsfun);
+// console.log(typeof 23);
+// console.log(typeof 'Imran');
+
+javascriptIsfun = 'YES!'
+console.log(typeof javascriptIsfun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+// const birthYear = 1990;
+// // birthYear = 1990;
+// // const job;
+// var job = 'programmer';
+// job = 'teacher';
+// lastName = 'Imran Hossain'
+// console.log(lastName);
+
+const now = 2037;
+const ageImran = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageImran, ageSarah);
+
+console.log(ageImran * 2, ageImran / 10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+
+const firstName = 'Imran';
+const lastName = 'Hossain';
+console.log(firstName + ' ' + lastName);
+
+
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+
+console.log(ageImran > ageSarah); //> , < , >= , <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageImran = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+let x, y;
+x = y = 25 - 10 - 5 // x = y = 10 , x = 10
+console.log(x, y);
